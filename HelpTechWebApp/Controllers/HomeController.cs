320517b75f87e2da635dc2e3b28d38e417997e18
aaron acuña <aaronacu_85@hotmail.com>
2025-07-22 14:27:56 -0500

    Agregar archivos de proyecto.

---
+++ HelpTechWebApp/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelpTechWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index() => View();
+
+        [HttpGet]
+        public IActionResult Error() => View();
+    }
+}
---
+++ HelpTechWebApp/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace HelpTechWebApp.Models
+{
+    public class User
+    {
+        public int Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        public User()
+        {
+            this.Username = 0;
+            this.Password = string.Empty;
+            this.Role = string.Empty;
+        }
+        public User(int username, string password, string role)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Role = role;
+        }
+    }
+}
